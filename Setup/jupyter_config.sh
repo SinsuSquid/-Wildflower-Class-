77ce92d25196b881b235d4c6f527bb89a42b755f
SinsuSquid <beomgyu.kang.kor@gmail.com>
2024-11-27 14:21:47 +0900

    Changed Dir Name

---
+++ Setup/jupyter_config.sh
@@ -0,0 +1,5 @@
+# Jupyter Notebook Aliases
+alias lab="jupyter lab --NotebookApp.token='' --NotebookApp.password='' --no-browser"
+alias notebook="jupyter notebook --NotebookApp.token ='' --NotebookApp.password='' --no-browser"
+
+# USAGE : lab --port=PORT or notebook --port=PORT
