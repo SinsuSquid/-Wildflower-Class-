08f8ee4084c1c211b9802a14b7645efc2a2dbf54
SinsuSquid <105333419+SinsuSquid@users.noreply.github.com>
2024-11-27 14:22:11 +0900

    Delete setup directory

--- setup/jupyter_config.sh
+++
@@ -1,5 +0,0 @@
-# Jupyter Notebook Aliases
-alias lab="jupyter lab --NotebookApp.token='' --NotebookApp.password='' --no-browser"
-alias notebook="jupyter notebook --NotebookApp.token ='' --NotebookApp.password='' --no-browser"
-
-# USAGE : lab --port=PORT or notebook --port=PORT
